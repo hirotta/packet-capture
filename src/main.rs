a0c3e0069b75f854236d6b8d32e7cd930a94704d
hirotta <nekopanda_dev@yahoo.co.jp>
2020-06-10 16:51:34 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use pnet::datalink;
+use pnet::datalink::Channel::Ethernet;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::ipv6::Ipv6Packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::udp::UdpPacket;
+use pnet::packet::Packet;
+#[macro_use]
+extern crate log;
+
+use std::env;
+
+mod packets;
+
+const WIDTH: usize = 20;
+
+fn main(){
+	
+}
